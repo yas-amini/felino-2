0eea883acc54f2ba65e0c214f9a587c4582914ed
yas-amini <yaasamin.amini@gmail.com>
2026-03-05 11:18:28 +0100

    add app routing and layout components

--- Frontend/client/src/components/booking/BookTable.tsx
+++
@@ -1,8 +0,0 @@
-export default function BookTable() {
-  return (
-    <section>
-      {/* Your old partials/book-table content goes here */}
-      <div>Book table form here</div>
-    </section>
-  );
-}
--- Frontend/client/src/components/booking/HandleBooking.tsx
+++
@@ -1,8 +0,0 @@
-export default function HandleBooking() {
-  return (
-    <section>
-      {/* Your old partials/handle-booking content goes here */}
-      <div>Handle booking form here</div>
-    </section>
-  );
-}
