a4210a3a8aa9b2c37b7043012374fb82be456070
Jessica Eriksson <jessica2-@hotmail.com>
2026-03-11 13:12:01 +0100

    feat: add booking calendar container layout

--- Frontend/client/src/components/admin/booking/BookingCalendar.tsx
+++ Frontend/client/src/components/admin/booking/BookingCalendar.tsx
@@ -1,20 +1,21 @@
+import "./BookingCalendar.css";
 
 export default function BookingCalendar() {
-    return (
-        <div style={{display:"flex"}}>
-
-        {/* Sidebar */}
-        <div>
+  return (
+    <div className="calendar-container">
+      <div className="calendar-layout">
+        <aside>
           <h3>Bokningar</h3>
           <p>17:00 Nilsson</p>
           <p>18:00 Andersson</p>
-        </div>
+        </aside>
 
-        {/* Calendar grid */}
-        <div>
+        <section className="calendar-area">
           <p>Här kommer kalendergrid</p>
-        </div>
+        </section>
 
       </div>
-    );
+    </div>
+
+  );
 }
