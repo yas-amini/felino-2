de9f70fc77910ee3c59971a34b2afe0b519d8116
Jessica Eriksson <jessica2-@hotmail.com>
2026-03-19 15:20:36 +0100

    Add BookingDtos

---
+++ Backend/Felino.Api/DTOs/Bookings/CreateBookingDto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Felino.Api.DTOs.Bookings;
+
+public class CreateBookingDto
+{
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [StringLength(20)]
+    public string Phone { get; set; } = null!;
+
+    [Required]
+    [EmailAddress]
+    [StringLength(150)]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    public DateOnly Date { get; set; }
+
+    [Required]
+    public TimeOnly Time { get; set; }
+
+    [Required]
+    [Range(1, 20)]
+    public int NumberOfGuests { get; set; }
+
+    [Required]
+    public bool OutdoorSeating { get; set; }
+
+    [StringLength(500)]
+    public string? SpecialRequests { get; set; }
+}
+
+
