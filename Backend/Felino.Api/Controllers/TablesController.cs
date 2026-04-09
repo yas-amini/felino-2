f02fefe1caeb243ac3d2760e40e2c69a65835e77
Jessica Eriksson <jessica2-@hotmail.com>
2026-04-09 14:35:41 +0200

    add tablecontroller

--- Backend/Felino.Api/Controllers/TablesController.cs
+++ Backend/Felino.Api/Controllers/TablesController.cs
@@ -1,6 +1,23 @@
-﻿namespace Felino.Api.Controllers
+﻿using Felino.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Felino.Api.Controllers;
+
+[ApiController]
+[Route("api/admin/tables")]
+public class AdminTablesController : ControllerBase
 {
-    public class TablesController
+    private readonly ITableService _tableService;
+
+    public AdminTablesController(ITableService tableService)
     {
+        _tableService = tableService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _tableService.GetAllTablesAsync();
+        return Ok(result);
     }
 }
