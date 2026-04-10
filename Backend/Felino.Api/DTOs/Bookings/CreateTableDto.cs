d8e0fe3e41ff8674d9a3664871e6e5df4494c838
Jessica Eriksson <jessica2-@hotmail.com>
2026-04-10 12:31:48 +0200

    add skapabord dto

---
+++ Backend/Felino.Api/DTOs/Bookings/CreateTableDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Felino.Api.DTOs.Bookings;
+
+public class CreateTableDto
+{
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [Range(1, 20)]
+    public int Capacity { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Placement { get; set; } = null!;
+}
